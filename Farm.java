fc9086adcd744e5edc0d36b8f2d47b3fbc677e7a
firebussy <eniola.odunuga@gompersstudent.org>
2018-04-16 14:26:40 -0700

    Add files via upload

---
+++ Farm.java
@@ -0,0 +1,45 @@
+
+public class Farm {
+	public static void main(String[] args) {
+	//1  Create a new random
+		
+	//7 Create a for loop that runs step 1-6, 20 times
+		
+	//2  Get a random number between 25-28
+		
+	//3  if the random number is 25
+		//call the cowSound method
+		
+	//4  if the random number is 256
+		//call the chickenSound method
+		
+	//5  if the random number is 27
+		//call the pigSound method
+		
+	//6  if the random number is 28
+		//call the catSound method
+		
+	
+	//7 Get another random number between 82-317
+		
+	//8  If the number is divisible by 3
+		//Print "THE ANIMALS ARE ESCAPING
+	
+	//9  else, print "You remember to lock the gate"
+		
+		
+	}
+	
+	static void cowSound(){
+		System.out.println("MOO");
+	}
+	static void chickenSound(){
+		System.out.println("CLUCK");
+	}
+	static void pigSound(){
+		System.out.println("OINK");
+	}
+	static void catSound(){
+		System.out.println("MEOW");
+	}
+}
